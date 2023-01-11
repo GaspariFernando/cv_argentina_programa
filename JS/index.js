80ca6f28e7aa4c0dbd16fd11ca645f07eddcc239
Fernando Gaspari <dev.gaspari.fernando@gmail.com>
2023-01-11 18:41:34 -0300

    cargue los datos academicos y laborales, cree la pagina sombre mi y añadi algo de estilo

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,5 @@
-console.log("Hola mundo!");
+
+
+function moreInfo() {
+    document.getElementById('p_alkemy').innerHTML = 'Manejo de Base de Datos, Metodología SCRUM, AWS S3, SendGrid, Spring-Boot, Spring Security, JUnit, Mockito, Arquitectura REST, Documentación con Swagger y Postman, Slack, Jira.';
+}
