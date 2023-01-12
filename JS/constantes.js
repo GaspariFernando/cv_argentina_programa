66fdbc5f30bd8efb49fddb4a6f55562c41cd7e6f
Fernando Gaspari <dev.gaspari.fernando@gmail.com>
2023-01-12 15:46:46 -0300

    finalizacion de index.html y comienzo de perfil.html

---
+++ JS/constantes.js
@@ -0,0 +1,18 @@
+const idBtnIfts = 'btn_ifts';
+const idBtnArgPrograma = 'btn_arg_programa';
+const idBtnPlatzi = 'btn_platzi';
+const idBtnAlkemy = 'btn_alkemy';
+const idBtnCodo = 'btn_codo';
+const idBtnSololearn = 'btn_sololearn';
+const idBtnUnla = 'btn_unla';
+const idBtnEscuela = 'btn_escuela';
+
+const infoIfts = 'Actualmente estoy cursando el curso de preingreso a la carrera "Tecnicatura SUPERIOR en Desarrollo de Software" del Instituto de Formación Técnica Superior, modalidad a distancia.';
+const infoArgPrograma = 'Actualmente estoy desarrollando un CV en la web con HTML, CSS y JavaScript como proyecto integrador.';
+const infoPlatzi = '<ul><li>Curso de Fundamentos de Angular. (<a href="https://platzi.com/p/dev.gaspari.fernando/curso/2478-angular/diploma/detalle/" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li><li>Curso de Angular: Componentes y Servicios. (<a href="https://platzi.com/p/dev.gaspari.fernando/curso/2486-course/diploma/detalle/" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li><li>Curso de Consumo de APIs REST con Angular. (<a href="https://platzi.com/p/dev.gaspari.fernando/curso/2495-angular-apis/diploma/detalle/" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li></ul>';
+const infoAlkemy = 'Manejo de Base de Datos, Metodología SCRUM, AWS S3, SendGrid, Spring-Boot, Spring Security, JUnit, Mockito, Arquitectura REST, Documentación con Swagger y Postman, Slack, Jira. (<a href="https://assets.alkemy.org/certificates/google-oauth2%7C105243236683909733966/8f4c50bf-5273-44b3-873f-57c99569a5a3.pdf" target="_blank" rel="noopener noreferrer">Ver Certificado</a>)';
+const infoCodo = 'HTML, CSS, Bootstrap, JavaScript, Vue, MySQL, Java, Spring-Boot, Postman.';
+const infoSololearn = '<ul><li>Certificado en HTML. (<a href="https://www.sololearn.com/Certificate/CT-WZ8ZMSPV/jpg" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li><li>Certificado en CSS. (<a href="https://www.sololearn.com/Certificate/CT-U32TWV6C/jpg" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li><li>Certificado en JavaScript. (<a href="https://www.sololearn.com/Certificate/CT-RZHKOU66/png" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li><li>Certificado en SQL. (<a href="https://www.sololearn.com/Certificate/CT-L4VQS4QH/png" target="_blank" rel="noopener noreferrer">Ver certificado</a>)</li></ul>';
+const infoUnla = 'Licenciatura en Sistemas.';
+const infoEscuela = 'Economía y gestión de las organizaciones.';
+
--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,25 @@
 
 
-function moreInfo() {
-    document.getElementById('p_alkemy').innerHTML = 'Manejo de Base de Datos, Metodología SCRUM, AWS S3, SendGrid, Spring-Boot, Spring Security, JUnit, Mockito, Arquitectura REST, Documentación con Swagger y Postman, Slack, Jira.';
-}
+function moreInfo(elementId) {
+
+    if (elementId === idBtnIfts) {
+        document.getElementById('p_ifts').innerHTML = infoIfts;
+    } else if (elementId === idBtnArgPrograma) {
+        document.getElementById('p_arg_programa').innerHTML = infoArgPrograma;
+    } else if (elementId === idBtnPlatzi) {
+        document.getElementById('p_platzi').innerHTML = infoPlatzi;
+    } else if (elementId === idBtnAlkemy) {
+        document.getElementById('p_alkemy').innerHTML = infoAlkemy;
+    } else if (elementId === idBtnCodo) {
+        document.getElementById('p_codo').innerHTML = infoCodo;
+    } else if (elementId === idBtnSololearn) {
+        document.getElementById('p_sololearn').innerHTML = infoSololearn;
+    } else if (elementId === idBtnUnla) {
+        document.getElementById('p_unla').innerHTML = infoUnla;
+    } else if (elementId === idBtnEscuela) {
+        document.getElementById('p_escuela').innerHTML = infoEscuela;
+    } else {
+        alert('No hay info para mostrar.');
+    }
+
+};
